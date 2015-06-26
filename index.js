c1a5ee9645c18b8f018a843aff4a53ffa53be894
Tim Schwecke <tim_schwecke@hotmail.com>
2015-06-26 09:25:52 -0600

    Missed one

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports.Scaffold = require('./lib/Scaffold.js');
 module.exports.Column = require('./lib/Column.js');
-module.exports.BlockCell = require('./lib/BlockCell.js');
+module.exports.ContentCell = require('./lib/ContentCell.js');
 module.exports.ColumnGroupCell = require('./lib/ColumnGroupCell.js');
